2ce50fc141a99ffad6f1ce8aaf85f968e334db4c
VadimKnyaz <86581586+VadimKnyaz@users.noreply.github.com>
2022-12-12 22:33:23 +0500

    Add files via upload

---
+++ Les091222.java
@@ -0,0 +1,13 @@
+public class Les091222 {
+    public static void main(String[] args) {
+        int[][] arra1= new int[8][5];
+        int[][] arra2 = new int[5][8];
+        System.out.println();
+        int [][]aray3=new int[5][];
+       int[] aray6= {1,2,3};
+       int [] arra4 = new int[2];
+       int [] array5 =new int []{1 ,2};
+
+    }
+
+}
