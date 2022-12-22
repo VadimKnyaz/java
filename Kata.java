1e3437b260f90e59240fe568b75e9a8d41acede5
VadimKnyaz <86581586+VadimKnyaz@users.noreply.github.com>
2022-12-22 06:57:58 +0500

    Add files via upload

---
+++ Kata.java
@@ -0,0 +1,9 @@
+public class Kata {
+    public static void main(String[] args) {
+        System.out.println(solution("world"));
+    }
+    public static String solution(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+}
